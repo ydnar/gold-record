77c7ebee1c316e27c49537a3b9b157e158876bbe
ydnar <ydnar@shaderlab.com>
2009-09-25 19:27:23 +0000

    More tests for #coerce_id.

--- test/cases/coerce_id_test.rb
+++ test/cases/coerce_id_test.rb
@@ -6,7 +6,19 @@ class CoerceIdTest < ActiveRecord::TestCase
     assert Artist.respond_to?(:coerce_id)
   end
 
+  def test_coerce_id_with_nil
+    assert_nil Artist.coerce_id(nil)
+  end
+
+  def test_coerce_id_with_bogus_data
+    assert_nil Artist.coerce_id("BOGUS")
+  end
+
   def test_coerce_id_with_binary_uuid
     assert_equal Artist.coerce_id(NULL_UUID_RAW), NULL_UUID_RAW
+  end
+
+  def test_coerce_id_with_hex_uuid
+    assert_equal Artist.coerce_id(NULL_UUID.to_s), NULL_UUID_RAW
   end
 end

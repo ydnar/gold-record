c9b6167d6fcdb2c3fde54e377bcef8ddd043709c
ydnar <ydnar@shaderlab.com>
2009-09-25 19:59:07 +0000

    Test #find with multiple hex UUIDs.

--- test/cases/find_test.rb
+++ test/cases/find_test.rb
@@ -25,4 +25,16 @@ class FindTest < ActiveRecord::TestCase
       assert_equal artist.class, Artist
     end
   end
+
+  def test_find_with_multiple_hex_uuids
+    artists = Artist.find([
+      identify_hex(:beatles),
+      identify_hex(:michael_jackson),
+      identify_hex(:warren_g),
+    ])
+    assert_equal artists.size, 3
+    artists.each do |artist|
+      assert_equal artist.class, Artist
+    end
+  end
 end

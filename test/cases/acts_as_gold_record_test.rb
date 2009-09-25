586590d3bfef4e569a44ca6737a9c318ca91e672
ydnar <ydnar@shaderlab.com>
2009-09-25 19:14:54 +0000

    More nuanced test.

--- test/cases/acts_as_gold_record_test.rb
+++ test/cases/acts_as_gold_record_test.rb
@@ -1,8 +1,13 @@
 require 'cases/helper'
+require 'models/label'
 require 'models/artist'
 
 class ActsAsGoldRecordTest < ActiveRecord::TestCase
-  def test_identify_as_gold_record
+  def test_label_does_not_identify_as_gold_record
+    assert !Label.respond_to?(:gold_record?)
+  end
+  
+  def test_artist_identifies_as_gold_record
     assert Artist.respond_to?(:gold_record?)
     assert Artist.gold_record?
   end

4cd28088323d626b588607b23b2d095e5024e04c
ydnar <ydnar@shaderlab.com>
2009-09-25 19:57:25 +0000

    Test #find with multiple binary UUIDs.

--- test/cases/find_test.rb
+++ test/cases/find_test.rb
@@ -13,4 +13,16 @@ class FindTest < ActiveRecord::TestCase
     artist = Artist.find identify_hex(:michael_jackson)
     assert_equal artist.class, Artist
   end
+
+  def test_find_with_multiple_binary_uuids
+    artists = Artist.find([
+      identify(:beatles),
+      identify(:michael_jackson),
+      identify(:warren_g),
+    ])
+    assert_equal artists.size, 3
+    artists.each do |artist|
+      assert_equal artist.class, Artist
+    end
+  end
 end

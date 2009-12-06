8328dbfb27bf7e2c53b01865e3eec3f528522b92
ydnar <ydnar@shaderlab.com>
2009-12-06 16:42:53 +0000

    Test find_some_with_uuid using Base64 variants.

--- test/cases/find_test.rb
+++ test/cases/find_test.rb
@@ -48,11 +48,23 @@ class FindTest < ActiveRecord::TestCase
     end
   end
 
+  def test_find_with_multiple_base64_uuids
+    artists = Artist.find([
+      identify_base64(:method_man),
+      identify_base64(:beatles),
+      identify_base64(:warren_g),
+    ])
+    assert_equal 3, artists.size
+    artists.each do |artist|
+      assert artist.instance_of?(Artist)
+    end
+  end
+
   def test_find_with_multiple_mixed_uuids
     artists = Artist.find([
-      identify_hex(:beatles),
-      identify(:michael_jackson),
-      identify_hex(:warren_g),
+      identify(:beatles),
+      identify_hex(:michael_jackson),
+      identify_base64(:warren_g),
     ])
     assert_equal 3, artists.size
     artists.each do |artist|
@@ -68,6 +80,9 @@ class FindTest < ActiveRecord::TestCase
       identify_hex(:beatles),
       identify_hex(:method_man),
       identify_hex(:warren_g),
+      identify_base64(:beatles),
+      identify_base64(:method_man),
+      identify_base64(:warren_g),
     ])
     assert_equal 3, artists.size
     artists.each do |artist|
